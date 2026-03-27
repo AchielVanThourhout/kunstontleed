915587353b2d9f01e3dfc4bc3dfe7cffdad94c42
Achiel Van Thourhout <achiel.vanthourhout@student.hogent.be>
2026-03-27 14:51:49 +0100

    aanpassingen zodat hij voldoende klaar is

---
+++ src/data/kunstenaars.js
@@ -0,0 +1,272 @@
+export const kunstenaars = [
+  {
+    naam: 'Bea Steen',
+    slug: 'bea-steen',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Carlos Caluwier',
+    slug: 'carlos-caluwier',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Christ Bruneel',
+    slug: 'christ-bruneel',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Christian Dedeene',
+    slug: 'christian-dedeene',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Daniel Dehulster',
+    slug: 'daniel-dehulster',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Daniel Joseph',
+    slug: 'daniel-joseph',
+    info: 'Arduin en metaal',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'David Smulders',
+    slug: 'david-smulders',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Els Vermandere',
+    slug: 'els-vermandere',
+    info: 'Marmer',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Erwin David',
+    slug: 'erwin-david',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Filiep Vandenberghe',
+    slug: 'filiep-vandenberghe',
+    info: 'Cortenstaal',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Frank Ranson',
+    slug: 'frank-ranson',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Geert Corneille (Kornul)',
+    slug: 'geert-corneille-kornul',
+    info: 'Brons',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Greet Decleir',
+    slug: 'greet-decleir',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Henk Korthuijs',
+    slug: 'henk-korthuijs',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'John Vandelanotte',
+    slug: 'john-vandelanotte',
+    info: 'Beeldjes WaTuKu',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Kathleen Boussauw',
+    slug: 'kathleen-boussauw',
+    info: 'Polyester stoel',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Koen Vandewalle',
+    slug: 'koen-vandewalle',
+    info: 'Hout',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Laurent Geers',
+    slug: 'laurent-geers',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Leen Clybouw',
+    slug: 'leen-clybouw',
+    info: 'Keramiek',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Louise Kerkaert',
+    slug: 'louise-kerkaert',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Luc Verbeke',
+    slug: 'luc-verbeke',
+    info: 'Marmer',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Maarten Van Thourhout',
+    slug: 'maarten-van-thourhout',
+    info: 'Papier',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Manon Huguenin',
+    slug: 'manon-huguenin',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Patrick Vanderstichele',
+    slug: 'patrick-vanderstichele',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Rita Craeynest',
+    slug: 'rita-craeynest',
+    info: 'Brons',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Stan Lievens',
+    slug: 'stan-lievens',
+    info: '',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Steven Selschotter',
+    slug: 'steven-selschotter',
+    info: 'Graffiti',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Theo Lefebvre',
+    slug: 'theo-lefebvre',
+    info: 'Keramiek',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Wim De Cauter',
+    slug: 'wim-de-cauter',
+    info: 'Franse witsteen',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  },
+  {
+    naam: 'Wim Franssens',
+    slug: 'wim-franssens',
+    info: 'Staal',
+    afbeelding: '',
+    wieIs: 'Info volgt later.',
+    kunstwerk: 'Info over het kunstwerk volgt later.',
+    contactgegevens: 'Contactgegevens volgen later.'
+  }
+];
